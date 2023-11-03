af61af035305e5350c4576e173835484a722ce65
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2023-11-03 07:38:09 -0600

    Video 7: Configuracion Inicial Zuul

--- springboot-service-celulares/src/main/java/com/microservice/celulares/controllers/CelularController.java
+++ springboot-service-celulares/src/main/java/com/microservice/celulares/controllers/CelularController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +21,10 @@ import com.microservice.celulares.service.CelularService;
 
 @RestController
 public class CelularController {
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private CelularService service;
 	
@@ -29,7 +34,7 @@ public class CelularController {
 	@GetMapping("/list")
 	public List<Celular> list() {
 		return service.findAll().stream().map(cel -> {
-			cel.setPort(port);
+			cel.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 			return cel;
 		}).collect(Collectors.toList());
 	}
