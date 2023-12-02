5c109df01ed06f65fab1d94d887f127e3e49586d
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2023-12-01 20:36:22 -0600

    Video 5: RepositoryRestResource HATEOAS

---
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gustavoo', '12345', 1, 'Gustavo', 'Rodriguez', 'gustavo@uanl.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('nissan', '12345', 1, 'Nissan', 'Gtr', 'nissan@uanl.com');
